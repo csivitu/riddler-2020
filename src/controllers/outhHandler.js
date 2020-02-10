9c611e22a9d775334889d5a569e8bc3f53257671
madrigal11 <kurupgokul11@gmail.com>
2020-02-10 22:11:34 +0530

    refactor:changs to the module in oauth

--- src/controllers/outhHandler.js
+++ src/controllers/outhHandler.js
@@ -1,30 +1,21 @@
-const express = require('express');
-const authorize = require('csi-accounts-express');
-const atob = require('atob');
+const express = require("express");
+const atob = require("atob");
 
 const app = express();
 const router = express.Router();
 
-const parseJwt = (token) => {
+const parseJwt = token => {
   try {
-    return JSON.parse(atob(token.split('.')[1]));
+    return JSON.parse(atob(token.split(".")[1]));
   } catch (e) {
     console.log(e);
     return null;
   }
 };
 
-app.use(authorize({
-    secret: process.env.JWT_SECRET,
-    token: (req, res, next) => {
-      return res.query.token; // If the token is stored in a session variable, for example
-    }
-}));
-
-router.get('/', (req, res) => {
-    const Info = parseJwt(req.query.token);
-    res.render('pages/loginSuccess',{user:Info});
+router.get("/", (req, res) => {
+  const Info = parseJwt(req.query.token);
+  res.render("pages/loginSuccess", { user: Info });
 });
-
 
 module.exports = router;

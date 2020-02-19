34958d9f20b6062183c9cc4167291c5160fc72db
KUHOO-S <kuhoo1999@rediffmail.com>
2020-02-19 12:47:45 +0530

    feat/favicon

--- src/static/js/questionsPage.js
+++ src/static/js/questionsPage.js
@@ -1,18 +1,34 @@
-$(document).ready(function () {
-    const backgroundMusic = new Audio("../static/audio/Nightcall  Instrumental_cutted.mp3");
+const backgroundMusic = new Audio('../static/audio/moosic.mp3');
+const clickMusic = new Audio('../static/audio/click.mp3');
 
-    backgroundMusic.play();
+let ammo = true;
+let count = 0;
+$(document.body).click(() => {
+    if (ammo) {
+        clickMusic.pause();
+        clickMusic.currentTime = 0;
+        clickMusic.play();
+        count += 1;
+    }
+    if (count > 50 && ammo) {
+        // eslint-disable-next-line no-alert
+        alert('OUT OF AMMO');
+        ammo = false;
+    }
+});
+backgroundMusic.addEventListener('ended', function autoplay() {
+    this.currentTime = 0;
+    this.play();
+}, false);
 
-    $("#vol").click(function () {
-        if ($("#vIcon").hasClass("fa-volume-up")) {
-            backgroundMusic.pause();
-            $("#vIcon").removeClass("fa-volume-up");
-            $("#vIcon").addClass("fa-volume-mute");
-
-        } else {
-            backgroundMusic.play();
-            $("#vIcon").addClass("fa-volume-up");
-            $("#vIcon").removeClass("fa-volume-mute");
-        }
-    });
+$('#vol').click(() => {
+    if ($('#vIcon').hasClass('fa-volume-up')) {
+        backgroundMusic.pause();
+        $('#vIcon').removeClass('fa-volume-up');
+        $('#vIcon').addClass('fa-volume-mute');
+    } else {
+        backgroundMusic.play();
+        $('#vIcon').addClass('fa-volume-up');
+        $('#vIcon').removeClass('fa-volume-mute');
+    }
 });

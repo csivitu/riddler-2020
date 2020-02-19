3eca7523ecd2d7b7c09b315aa6c4037a02d3f141
Rohan Mukherjee <rohanbayern@gmail.com>
2020-02-19 23:14:14 +0530

    feat: Fixed some linting issues

--- src/static/js/trackSelectorHandler.js
+++ src/static/js/trackSelectorHandler.js
@@ -1,36 +1,35 @@
 window.onload = () => {
     const tracksDOM = document.getElementsByTagName('button');
-    const question = document.querySelector("#question");
+    const question = document.querySelector('#question');
 
-    //Starter questiosn are hardcoded   
-    const questions = ["a", "b", "c"];
+    // Starter questiosn are hardcoded
+    const questions = ['a', 'b', 'c'];
     const tracks = Object.values(tracksDOM);
     console.log(tracks);
     tracks.forEach((btn) => {
         btn.addEventListener('click', (e) => {
-            const trackSelected;
-            for (let i = 0; i < tracks.length; i++) {
-                if (tracks[i] === e.target)
-                    trackSelected = i;
+            let trackSelected = 1;
+            for (let i = 0; i < tracks.length; i += 1) {
+                if (tracks[i] === e.target) trackSelected = i;
             }
 
             let riddleID;
             switch (trackSelected) {
-                case 0:
-                    riddleID = 'A0';
-                    break;
-                case 1:
-                    riddleID = 'B0';
-                    break;
-                case 2:
-                    riddleID = 'C0';
-                    break;
-                default:
-                    riddleID = 'A0';
+            case 0:
+                riddleID = 'A0';
+                break;
+            case 1:
+                riddleID = 'B0';
+                break;
+            case 2:
+                riddleID = 'C0';
+                break;
+            default:
+                riddleID = 'A0';
             }
-            p.innerHTML = questions[trackSelected];
+            question.innerHTML = questions[trackSelected];
             console.log(riddleID);
-            const url = `${location.origin}/game?id=${encodeURI(riddleID)}`;
+            const url = `${window.location.origin}/game?id=${encodeURI(riddleID)}`;
             console.log(url);
 
             window.location.href = url;

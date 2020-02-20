3902da4a443ee354afc6d04480f7c3feb93b1a47
thebongy <rishit.bansal0@gmail.com>
2020-02-21 00:50:05 +0530

    fix: Deploy

--- src/middlewares/verifyUser.js
+++ src/middlewares/verifyUser.js
@@ -6,12 +6,14 @@ const verifyUser = async (req, res, next) => {
 
     const userExists = await User.findOne({ username: currentUser.username });
     req.riddlerUser = userExists;
-
-    if (!req.riddlerUser.currentRiddle || !req.riddlerUser.mainTrackProgress || !req.riddlerUser.points || typeof req.riddlerUser.hintsUsed !== 'object') {
+    if (req.riddlerUser.currentRiddle === undefined
+        || req.riddlerUser.mainTracksProgress === undefined
+        || req.riddlerUser.points === undefined) {
         req.riddlerUser.currentRiddle = '0';
-        req.riddlerUser.mainTrackProgress = ['A0', 'B0', 'C0'];
+        req.riddlerUser.mainTracksProgress = ['A0', 'B0', 'C0'];
         req.riddlerUser.points = 300;
         req.riddlerUser.hintsUsed = [];
+        req.riddlerUser.markModified('mainTracksProgress');
         await req.riddlerUser.save();
     }
     if (!userExists) return res.render('error', { error: 'Not even Donald Trump is allowed in here !' });

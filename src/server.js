d3ab64768d2240906f53e55c38dd740309dec062
thebongy <rishit.bansal0@gmail.com>
2020-02-21 01:18:38 +0530

    fix: Remove admin route

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 const register = require('./routes/register.js');
 const callback = require('./routes/accountsCallback.js');
 const game = require('./routes/game.js');
-const admin = require('./routes/admin.js');
+// const admin = require('./routes/admin.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -35,7 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/', register);
 app.use('/oauth/redirect', callback);
 app.use('/game', game);
-app.use('/admin', admin);
+// app.use('/admin', admin);
 
 app.use('*', (req, res) => {
     res.render('error', { error: `${req.originalUrl} is not defined` });
